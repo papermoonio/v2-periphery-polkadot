b24991df722ba9af9fc317b71d4a858823b405af
sekisamu <22696121+sekisamu@users.noreply.github.com>
2025-10-22 16:39:26 +0800

    add builtin accounts in local config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,8 +50,8 @@ const config: HardhatUserConfig = {
       // polkavm: true,
       url: 'http://127.0.0.1:8545',
       accounts: [
-        process.env.LOCAL_PRIV_KEY as string,
-        process.env.AH_PRIV_KEY as string,
+        '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133',
+        '0x8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b'
       ],
     },
     ah: {
